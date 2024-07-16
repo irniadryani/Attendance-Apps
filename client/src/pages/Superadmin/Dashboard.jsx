b276d317e30a3e88ddd4549e8cdea23f94cd1147
Irni Dwi Adryani <irnidwi16@gmail.com>
2024-07-16 09:24:05 +0700

    feat: add fe create account pages for superadmin

--- client/src/pages/Superadmin/Dashboard.jsx
+++ client/src/pages/Superadmin/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../Layout'
+import CreateAccount from '../../components/Superadmin/CreateAccount'
 
 export default function Dashboard() {
   return (
     <Layout>
-      <div>Dashboard</div>
+      <CreateAccount/>
     </Layout>
   )
 }
