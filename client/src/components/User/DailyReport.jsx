c29f6167c45d2dcce4155950ff9355c1ade0d112
Irni Dwi Adryani <irnidwi16@gmail.com>
2024-08-15 07:51:58 +0700

    feat: change date picker with range date

--- client/src/components/User/DailyReport.jsx
+++ client/src/components/User/DailyReport.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import { getDailyReportByIdFn } from "../../api/dailyReport/dailyReport.js";
 import { useQuery } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRange } from "react-date-range";
 
 export default function DailyReport() {
   const { user } = useSelector((state) => state.auth);
+  const [dateRange, setDateRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [showDateRange, setShowDateRange] = useState(false);
+  const [isDateRangeCleared, setIsDateRangeCleared] = useState(true);
 
   const {
     data: dataSingleDailyReport,
@@ -14,9 +26,31 @@ export default function DailyReport() {
   } = useQuery({
     queryKey: ["dailyReport", user?.id],
     queryFn: () => getDailyReportByIdFn(user?.id),
+    enabled: !!user.id,
   });
 
-  console.log("Daily Report Data:", dataSingleDailyReport);
+  const filteredReports = dataSingleDailyReport?.filter((report) => {
+    const reportDate = new Date(report.report_date);
+    if (isDateRangeCleared) {
+      return true; // Show all reports when date range is cleared
+    }
+    return reportDate >= dateRange[0].startDate && reportDate <= dateRange[0].endDate;
+  });
+  const handleClear = () => {
+    setDateRange([
+      {
+        startDate: new Date(),
+        endDate: new Date(),
+        key: "selection",
+      },
+    ]);
+    setIsDateRangeCleared(true);
+    refetchDailyReport(); // Clear and fetch again
+  };
+
+  const toggleDateRangePicker = () => {
+    setShowDateRange(!showDateRange);
+  };
 
   return (
     <div>
@@ -28,24 +62,50 @@ export default function DailyReport() {
             </button>
           </form>
           <h3 className="font-bold text-lg">Daily Report</h3>
-          <div className="flex my-5">
-            <Accordion variant="splitted" className="dark:bg-gray-800 text-black">
-            {dataSingleDailyReport?.map((dailyReport) => (
-              <AccordionItem
-                key={dailyReport.id}
-                aria-label={`Accordion ${dailyReport.id}`}
-                subtitle={dailyReport.report_date} 
-              >
-                <div className="flex flex-col gap-1">
-                  <p className="text-sm font-semibold">
-                    {dailyReport.report_message}
-                  </p>
-                </div>
-              </AccordionItem>
-            ))}
-          </Accordion>
+          <div>
+            {showDateRange && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => {
+                  setDateRange([item.selection]);
+                  setIsDateRangeCleared(false);
+                }}
+                moveRangeOnFirstSelection={false}
+                ranges={dateRange}
+              />
+            )}
           </div>
-          
+          <div className="flex justify-between my-4">
+            <button
+              onClick={toggleDateRangePicker}
+              className="btn bg-black text-white"
+            >
+              {showDateRange ? "Hide Date Range" : "Show Date Range"}
+            </button>
+            <button onClick={handleClear} className="btn bg-red-500 text-white">
+              Clear
+            </button>
+          </div>
+          <div className="flex my-5">
+            <Accordion
+              variant="splitted"
+              className="dark:bg-gray-800 text-black"
+            >
+              {filteredReports?.map((dailyReport) => (
+                <AccordionItem
+                  key={dailyReport.id}
+                  aria-label={`Accordion ${dailyReport.id}`}
+                  subtitle={dailyReport.report_date}
+                >
+                  <div className="flex flex-col gap-1">
+                    <p className="text-sm font-semibold">
+                      {dailyReport.report_message}
+                    </p>
+                  </div>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
         </div>
       </dialog>
     </div>
--- client/src/pages/Admin/DetailDailyReport.jsx
+++ client/src/pages/Admin/DetailDailyReport.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Layout from "../Layout";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getDailyReportByIdFn } from "../../api/dailyReport/dailyReport";
 import { Accordion, AccordionItem } from "@nextui-org/react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRange } from "react-date-range";
 
 export default function DetailDailyReport() {
-  const [date, setDate] = useState(null);
   const { id } = useParams();
   const location = useLocation();
   const employeeId = atob(id);
@@ -17,22 +17,58 @@ export default function DetailDailyReport() {
     employeePosition: null,
   };
 
-  const { data: dataSingleDailyReport, isLoading: loadingSingleDailyReport } =
+  const [dateRange, setDateRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [showDateRange, setShowDateRange] = useState(false);
+  const [isDateRangeCleared, setIsDateRangeCleared] = useState(true);
+
+  const { data: dataSingleDailyReport, isLoading: loadingSingleDailyReport, refetch: refetchDailyReport } =
     useQuery({
       queryKey: ["dailyReport", employeeId],
       queryFn: () => getDailyReportByIdFn(employeeId),
     });
 
-  const sortedDailyReports = dataSingleDailyReport?.sort((a, b) => {
-    return new Date(a.report_date) - new Date(b.report_date);
+  const handleClear = () => {
+    setDateRange([
+      {
+        startDate: new Date(),
+        endDate: new Date(),
+        key: "selection",
+      },
+    ]);
+    setIsDateRangeCleared(true);
+    refetchDailyReport(); // Clear and fetch again
+  };
+
+  const toggleDateRangePicker = () => {
+    setShowDateRange(!showDateRange);
+  };
+
+  const filteredReports = dataSingleDailyReport?.filter((report) => {
+    const reportDate = new Date(report.report_date);
+    if (isDateRangeCleared) {
+      return true; // Show all reports when date range is cleared
+    }
+    return reportDate >= dateRange[0].startDate && reportDate <= dateRange[0].endDate;
   });
 
-  const filteredDailyReport = sortedDailyReports?.filter((dailyReport) => {
-    const matchingDate =
-      !date ||
-      new Date(dailyReport?.report_date).toDateString() === date.toDateString();
-    return matchingDate;
-  });
+  // const sortedDailyReports = dataSingleDailyReport?.sort((a, b) => {
+  //   return new Date(a.report_date) - new Date(b.report_date);
+  // });
+
+  // const filteredDailyReport = sortedDailyReports?.filter((dailyReport) => {
+  //   const reportDate = new Date(dailyReport.report_date);
+  //   const startDate = dateRange[0].startDate;
+  //   const endDate = dateRange[0].endDate;
+
+  //   // Check if the report date falls within the selected date range
+  //   return reportDate >= startDate && reportDate <= endDate;
+  // });
 
   return (
     <Layout>
@@ -40,28 +76,34 @@ export default function DetailDailyReport() {
         <p className="mt-10 font-bold text-4xl text-black">{employeeName}</p>
         <p className="font-medium text-xl text-black">{employeePosition}</p>
       </div>
-      <div className="mt-10">
-        <div className="flex flex-row p-3 bg-base shadow-lg text-black w-72 rounded-xl">
-          <div>
-            <p className="font-semibold text-sm items-center mt-1">
-              Sort By Date:
-            </p>
-          </div>
-
-          <div className="ml-1 w-36 rounded-lg">
-            <DatePicker
-            className="rounded-lg text-black text-xs"
-              selected={date}
-              onChange={(date) => setDate(date)}
-              isClearable
-              placeholderText=" Select a date"
-            />
-          </div>
-        </div>
+      <div>
+        {showDateRange && (
+          <DateRange
+            editableDateInputs={true}
+            onChange={(item) => {
+              setDateRange([item.selection]);
+              setIsDateRangeCleared(false);
+              refetchDailyReport(); // Refetch data when date range changes
+            }}
+            moveRangeOnFirstSelection={false}
+            ranges={dateRange}
+          />
+        )}
+      </div>
+      <div className="flex justify-between my-4">
+        <button
+          onClick={toggleDateRangePicker}
+          className="btn bg-black text-white"
+        >
+          {showDateRange ? "Hide Date Range" : "Show Date Range"}
+        </button>
+        <button onClick={handleClear} className="btn bg-red-500 text-white">
+          Clear
+        </button>
       </div>
       <div className="my-5">
         <Accordion variant="splitted" className="dark:bg-gray-800 text-black">
-          {filteredDailyReport?.map((dailyreport) => (
+          {filteredReports?.map((dailyreport) => (
             <AccordionItem
               key={dailyreport.id}
               aria-label={`Accordion ${dailyreport.id}`}
